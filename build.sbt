d273104008aa4cb57287720c1178f5773b02e0da
DaniSancas <lord.sancas@gmail.com>
2019-11-29 22:31:03 +0100

    Logging implemented
    TODO Tests

--- build.sbt
+++ build.sbt
@@ -3,10 +3,13 @@ name := "envelope"
 version := "0.1"
 
 scalaVersion := "2.12.10"
+
 libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-lambda-java-events" % "2.2.5",
   "com.amazonaws" % "aws-lambda-java-core" % "1.2.0",
-  "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.8" % "test",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 )
 
 val circeVersion = "0.10.0"
--- src/main/scala/io/github/danisancas/envelope/boilerplate/SimpleTextMessage.scala
+++ src/main/scala/io/github/danisancas/envelope/boilerplate/SimpleTextMessage.scala
@@ -1,6 +1,7 @@
 package io.github.danisancas.envelope.boilerplate
 
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
+import com.typesafe.scalalogging.LazyLogging
 import io.circe.parser.decode
 import io.github.danisancas.envelope.{TOutMessage, TUpdate, TelegramWrapper}
 
@@ -9,7 +10,9 @@ import scala.util.{Failure, Success, Try}
 
 case class TextMessage(chatId: Int, text: String, messageId: Int)
 
-object SimpleTextMessage extends TelegramWrapper[APIGatewayProxyRequestEvent, TUpdate, TextMessage, TOutMessage, APIGatewayProxyResponseEvent]{
+object SimpleTextMessage
+  extends TelegramWrapper[APIGatewayProxyRequestEvent, TUpdate, TextMessage, TOutMessage, APIGatewayProxyResponseEvent]
+  with LazyLogging {
 
   import io.circe.Printer
   import io.circe.generic.auto._
@@ -18,18 +21,29 @@ object SimpleTextMessage extends TelegramWrapper[APIGatewayProxyRequestEvent, TU
   private val printer: Printer = Printer.noSpaces.copy(preserveOrder = true, dropNullValues = true)
 
   override def decodeInput(input: String): Try[TUpdate] = {
-    decode[TUpdate](input).toTry // Decode request using circe
+    logger.info("Decoding input JSON to TUpdate case class")
+    logger.debug(s"Input: $input")
+    val result = decode[TUpdate](input).toTry // Decode request using circe
+    logger.debug(s"Decoded: $result")
+    result
 }
 
   override def convertToMessage(update: TUpdate): Try[TextMessage] = {
+    logger.info("Converting TUpdate case class to TextMessage case class")
+    logger.debug(s"TUpdate: $update")
     val result = for {
       msg <- update.message
       text <- msg.text
     } yield TextMessage(msg.chat.id, text, msg.message_id)
 
     result match {
-      case Some(a) => Success(a)
-      case None => Failure(new Exception("Couldn't convert to simple TextMessage"))
+      case Some(a) =>
+        logger.debug(s"Converted: $result")
+        Success(a)
+      case None =>
+        val error = "Couldn't convert to simple TextMessage"
+        logger.error(error)
+        Failure(new Exception(error))
     }
   }
 
@@ -48,35 +62,42 @@ object SimpleTextMessage extends TelegramWrapper[APIGatewayProxyRequestEvent, TU
   }
 
   def sendOkResponse(message: TOutMessage): APIGatewayProxyResponseEvent = {
+    logger.info("Sending Ok Response")
     val response = new APIGatewayProxyResponseEvent() // Example for a response
       .withStatusCode(200)
       .withHeaders(Map("Content-Type" -> "application/json").asJava)
       .withBody(printer.pretty(message.asJson))
 
-    println(s"Body: $message")
-    println(s"Response: $response")
+    logger.debug(s"Body: $message")
+    logger.debug(s"Response: $response")
 
     response
   }
 
   def sendErrorResponse(error: String): APIGatewayProxyResponseEvent = {
+    logger.info("Sending Error Response")
     val response = new APIGatewayProxyResponseEvent() // Example for a response
       .withStatusCode(200)
       .withBody(s"Error: $error")
 
-    println(s"Response: $response")
+    logger.debug(s"Response: $response")
 
     response
   }
 
   def stripCommand(text: String): Option[(String, String)] = {
+    logger.info(s"Parsing command for string '$text'")
     val cmdRegex = "(\\/[a-zA-Z0-9]+)? *(.*)".r
 
-    text match {
+    val result = text match {
       case cmdRegex(cmd, rest) if cmd == null && rest == "" => Option.empty
       case cmdRegex(cmd, rest) if cmd == null => Some("", rest)
       case cmdRegex(cmd, rest) => Some(cmd, rest)
       case _ => Option.empty
     }
+
+    logger.debug(s"Parsed result: $result")
+
+    result
   }
 }
